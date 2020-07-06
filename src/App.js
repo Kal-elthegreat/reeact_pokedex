c14fe363f0d53112b948838730d229db6fbd8712
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-07-06 09:33:14 -0400

    setup of pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from './components/Header';
 
 function App() {
+  const [name, setName] = useState(null);
+  const [img, setImg] = useState(null);
+  // const [results, setResults] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await fetch("https://pokeapi.co/api/v2/pokemon/3") //?limit=151
+          .then((res) => res.json())
+          .then((data) => {
+            setImg(data.sprites.front_default);
+            setName(data.name);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokedex">
+      <Header /> {/* need a pokemon style font */}
+      <div className="display">
+        <h1>{name}</h1> 
+        <img src={img} alt={name} style={{ width: 175, height: 175 }} />
+      </div>
+
+        <div className="controls">
+          <button style={{width: 100}}>Previous</button>
+          <button style={{ width: 100 }}>Next</button>
+        </div>
+
     </div>
   );
 }
