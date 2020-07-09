6de928e4f9e1449ff9138d5c819c0ab928f10cd3
Kiel Lewis <kiel.lewis@mailchimp.com>
2020-07-08 23:18:22 -0400

    can get an id to switch between pokemon

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Header from './components/Header';
+import Header from "./components/Header";
 
 function App() {
   const [name, setName] = useState(null);
   const [img, setImg] = useState(null);
-  // const [results, setResults] = useState(null);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        await fetch("https://pokeapi.co/api/v2/pokemon/3") //?limit=151
-          .then((res) => res.json())
-          .then((data) => {
-            setImg(data.sprites.front_default);
-            setName(data.name);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchData();
-  }, []);
+
+  const getPoke = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`) //?limit=151
+      .then((res) => res.json())
+      .then((data) => {
+        setImg(data.sprites.front_default);
+        setName(data.name);
+      });
+  };
+  console.log(getPoke(3));
 
   return (
     <div className="pokedex">
       <Header /> {/* need a pokemon style font */}
       <div className="display">
-        <h1>{name}</h1> 
+        <h1>{name}</h1>
         <img src={img} alt={name} style={{ width: 175, height: 175 }} />
       </div>
-
-        <div className="controls">
-          <button style={{width: 100}}>Previous</button>
-          <button style={{ width: 100 }}>Next</button>
-        </div>
-
+      <div className="controls">
+        <button style={{ width: 100 }}>Previous</button>
+        <button style={{ width: 100 }}>Next</button>
+      </div>
     </div>
   );
 }
